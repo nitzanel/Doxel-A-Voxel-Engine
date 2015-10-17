e41941943c6ea4fe2f84096173179bfff9d109ca
Nitzan Elbaz <nitzanel2@gmail.com>
2015-10-17 16:57:22 +0300

    test

--- Doxel/Doxel.h
+++ Doxel/Doxel.h
@@ -2,8 +2,9 @@
 
 #define BLOCK_WIDTH 1
 #define CHUNK_SIZE  64
-#define NUM_CHUNKS 8
-#define RENDER_DISTANCE 100.0f
+#define NUM_CHUNKS 4
+
+#define RENDER_DISTANCE 200.0f
 
 #define EPSILON 0.001
 #define PI 3.141592f
